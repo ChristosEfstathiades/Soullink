96b5520f95197011d7ef44cd44e88c8894d4f381
d86617ce <chrise3012@icloud.com>
2025-11-06 21:41:45 +0000

    glow effect for selected wallpaper in settings

--- resources/js/components/soullink/pc-box-background-options.tsx
+++ resources/js/components/soullink/pc-box-background-options.tsx
@@ -9,7 +9,7 @@ export default function PCBoxBackgroundOptions() {
     return (
         <div>
             {Array.from({ length: 9 }).map((_, index) => (
-                <div key={index} className={`inline-block rounded-xl cursor-pointer border-4 ${pcBoxBackground === index ? 'border-[#ff1c1c]' : 'border-transparent'} m-2`} onClick={() => setPcBoxBackground(index)}>
+                <div key={index} className={`inline-block rounded-xl cursor-pointer ${pcBoxBackground === index ? ' shadow-[0_0_25px_7px_rgba(255,28,28,1)]' : 'border-transparent'} m-2`} onClick={() => setPcBoxBackground(index)}>
                     <img src={`/storage/pc-box-backgrounds/${index}.png`} alt={`PC Box Background ${index}`} className="w-25 sm:w-40" />
                 </div>
             ))}
