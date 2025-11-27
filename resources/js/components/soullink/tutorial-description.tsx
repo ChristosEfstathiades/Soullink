ced990240584d533ab6dec86e6e856995c2c6302
d86617ce <chrise3012@icloud.com>
2025-11-27 17:55:00 +0000

    tutorial section content

--- resources/js/components/soullink/tutorial-description.tsx
+++ resources/js/components/soullink/tutorial-description.tsx
@@ -1,7 +1,7 @@
 export default function TutorialDescription({ children }: { children: React.ReactNode }) {
     return (
         <>
-            <p className="mt-2 text-white">{children}</p>
+            <p className="mt-2 text-sm text-white">{children}</p>
         </>
     );
 }
